a1790d2cc2dc5c8f67899cbb1c93a616979662d6
Sweet Van Loan <Sweet@SweetVanLoan.com>
2023-12-21 11:32:53 -0600

    Create item.cpp

---
+++ item.cpp
@@ -0,0 +1,11 @@
+#include "Item.h"
+
+int Item::getHPIncrease() const {
+    switch (gemType) {
+        case GemType::Red: return 10;
+        case GemType::Green: return 20;
+        case GemType::Purple: return 30;
+        case GemType::Yellow: return -1; // Special case for full HP
+        default: return 0;
+    }
+}
