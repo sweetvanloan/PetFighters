906c8e43f7d7c1e6f037d629c0808e0d44cecd43
Sweet Van Loan <Sweet@SweetVanLoan.com>
2023-12-21 11:30:01 -0600

    Update and rename Reproduction to Pet.cpp

---
+++ Pet.cpp
@@ -0,0 +1,62 @@
+#include "Pet.h"
+#include <iostream>
+
+Pet::Pet(const std::string& petName, ElementType petElement)
+    : name(petName), health(100), happiness(100), level(1), element(petElement),
+      currentHP(100), baseMaxHP(100), enhancedMaxHP(100) {}
+
+void Pet::setName(const std::string& newName) {
+    name = newName;
+}
+
+std::string Pet::getName() const {
+    return name;
+}
+
+void Pet::setElement(ElementType newElement) {
+    element = newElement;
+}
+
+ElementType Pet::getElement() const {
+    return element;
+}
+
+void Pet::increaseHP(int amount) {
+    currentHP = std::min(currentHP + amount, enhancedMaxHP);
+}
+
+void Pet::decreaseHP(int amount) {
+    currentHP = std::max(currentHP - amount, 0);
+}
+
+void Pet::enhanceMaxHP(int amount) {
+    enhancedMaxHP += amount;
+    if (enhancedMaxHP > baseMaxHP) {
+        baseMaxHP = enhancedMaxHP;
+    }
+}
+
+int Pet::getCurrentHP() const {
+    return currentHP;
+}
+
+int Pet::getMaxHP() const {
+    return enhancedMaxHP;
+}
+
+void Pet::useElementalAttack() const {
+    // Implementation of elemental attack
+}
+
+void Pet::useElementalDefense() const {
+    // Implementation of elemental defense
+}
+
+void Pet::useItem(const Item& item) {
+    int hpIncrease = item.getHPIncrease();
+    if (hpIncrease == -1) {
+        currentHP = enhancedMaxHP; // Full HP for yellow gem
+    } else {
+        increaseHP(hpIncrease);
+    }
+}
