a19ae60c930e55a6a66f8275ff39cfc4de4c8bef
Sweet Van Loan <Sweet@SweetVanLoan.com>
2023-12-21 11:29:12 -0600

    Update and rename PetClass to pet.h

---
+++ pet.h
@@ -0,0 +1,43 @@
+#ifndef PET_H
+#define PET_H
+
+#include <string>
+#include "Item.h"
+
+enum class ElementType {
+    Fire, Water, Earth, Air // Extend as needed
+};
+
+class Pet {
+private:
+    std::string name;
+    int health;
+    int happiness;
+    int level;
+    ElementType element;
+    int currentHP;
+    int baseMaxHP;
+    int enhancedMaxHP;
+
+public:
+    Pet(const std::string& petName = "Unnamed Pet", ElementType petElement = ElementType::Fire);
+
+    void setName(const std::string& newName);
+    std::string getName() const;
+
+    void setElement(ElementType newElement);
+    ElementType getElement() const;
+
+    void increaseHP(int amount);
+    void decreaseHP(int amount);
+    void enhanceMaxHP(int amount);
+    int getCurrentHP() const;
+    int getMaxHP() const;
+
+    void useElementalAttack() const;
+    void useElementalDefense() const;
+
+    void useItem(const Item& item);
+};
+
+#endif
