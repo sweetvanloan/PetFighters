b065199bb15b071453ed9227e9b356b1151a80fc
Sweet Van Loan <Sweet@SweetVanLoan.com>
2023-12-21 11:33:32 -0600

    Create game.h

---
+++ game.h
@@ -0,0 +1,19 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "Pet.h"
+#include <vector>
+
+class Game {
+private:
+    std::vector<Pet> pets;
+    std::vector<Pet*> favorites;
+
+public:
+    void addPet(const Pet& newPet);
+    void addToFavorites(const std::string& petName);
+    void removeFromFavorites(const std::string& petName);
+    // Other game methods...
+};
+
+#endif
