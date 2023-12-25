f6e96a170af83a831147894636de336781a44867
Sweet Van Loan <Sweet@SweetVanLoan.com>
2023-12-25 11:25:25 -0600

    Create player.cpp

---
+++ player.cpp
@@ -0,0 +1,25 @@
+#include "Player.h"
+
+void Player::setName(const std::string& newName) {
+    name = newName;
+}
+
+void Player::addPet(const Pet& pet) {
+    pets.push_back(pet);
+}
+
+void Player::setupBattleTeam(const std::vector<int>& petIndices) {
+    battleTeam.clear();
+    for (int index : petIndices) {
+        if (index >= 0 && index < pets.size()) {
+            battleTeam.push_back(&pets[index]);
+        }
+        if (battleTeam.size() == 4) {
+            break;
+        }
+    }
+}
+
+void Player::interactWithPet(const std::string& petName, int actionType) {
+    // Implement interaction logic...
+}
