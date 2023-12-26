c00e5c1438c2f544feefabb4b76e8ffe9ac167cc
Sweet Van Loan <Sweet@SweetVanLoan.com>
2023-12-26 14:24:57 -0600

    Update pet.h
    Adding moods

--- pet.h
+++ pet.h
@@ -4,8 +4,14 @@
 #include <string>
 #include "Item.h"
 
+//  Elemental Types
 enum class ElementType {
     Fire, Water, Earth, Air, Lightning, Wind, Ice
+};
+
+// Pet Moods
+enum class Mood {
+    Happy, Sad, Excited, Relaxed // Extend with additional moods
 };
 
 class Pet {
@@ -14,27 +20,38 @@ private:
     int health, happiness, level;
     ElementType element;
     int currentHP, baseMaxHP, enhancedMaxHP;
+    Mood currentMood;
 
 public:
+    // Constructor
     Pet(const std::string& petName = "Unnamed Pet", ElementType petElement = ElementType::Fire);
 
+    // Setters and getters
     void setName(const std::string& newName);
     std::string getName() const;
-
     void setElement(ElementType newElement);
     ElementType getElement() const;
+    Mood getCurrentMood() const;
 
+    // HP management
     void increaseHP(int amount);
     void decreaseHP(int amount);
     void enhanceMaxHP(int amount);
     int getCurrentHP() const;
     int getMaxHP() const;
 
+    // Elemental abilities
     void useElementalAttack() const;
     void useElementalDefense() const;
 
+    // Item usage
     void useItem(const Item& item);
 
+    // Mood and interaction
+    void interactWithOtherPet(Pet& otherPet);
+    void changeMood(Mood newMood);
+
+    // Happiness
     void increaseHappiness(int amount);
     void decreaseHappiness(int amount);
     void updateStatus();
