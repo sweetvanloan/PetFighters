61d8dca4bcc912c6a1f594e4a19b53b85a0925b5
Sweet Van Loan <Sweet@SweetVanLoan.com>
2023-12-30 08:10:18 -0600

    Update main.cpp
    Start and Load pages

--- main.cpp
+++ main.cpp
@@ -1,9 +1,49 @@
 #include "Game.h"
 #include <iostream>
+#include <string>
+
+void displayStartPage() {
+    std::cout << "Welcome to Pet Adventure Game!\n";
+    std::cout << "1. Start New Game\n";
+    std::cout << "2. Load Game\n";
+    std::cout << "3. Exit\n";
+    std::cout << "Enter your choice: ";
+}
+
+void startNewGame(Game& game) {
+    // Logic to start a new game
+}
+
+void loadGame(Game& game) {
+    // Logic to load a game
+}
+
+
+
 
 int main() {
     Game game;
     Player player;
+
+    bool exitGame = false;
+
+    while (!exitGame) {
+        displayStartPage();
+        int choice;
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                startNewGame(game);
+                break;
+            case 2:
+                loadGame(game);
+                break;
+            case 3:
+                exitGame = true;
+                break;
+            default:
+                std::cout << "Invalid choice. Please try again.\n";
 
     std::string playerName;
     std::cout << "Enter your player name: ";
